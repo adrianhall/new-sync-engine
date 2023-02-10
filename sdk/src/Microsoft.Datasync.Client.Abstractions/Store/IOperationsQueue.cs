09353f6748f28fce14d9d93c70572c7fbe093e41
Adrian Hall <photoadrian@outlook.com>
2023-02-10 15:45:55 -0800

    Exit from vs

--- sdk/src/Microsoft.Datasync.Client.Abstractions/Store/IOperationsQueue.cs
+++ sdk/src/Microsoft.Datasync.Client.Abstractions/Store/IOperationsQueue.cs
@@ -10,11 +10,4 @@ public interface IOperationsQueue
     /// by the created data.
     /// </summary>
     IQueryable<IDatasyncOperation> AsQueryable();
-
-    /// <summary>
-    /// Returns the next operation that needs to be transmitted to the remote service.
-    /// </summary>
-    /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe.</param>
-    /// <returns>The next operation, or <c>null</c> if nothing is enqueued.</returns>
-    ValueTask<IDatasyncOperation?> GetNextOperationAsync(CancellationToken cancellationToken = default);
 }
--- sdk/test/Test.Client.EntityFrameworkCore/OperationsQueue_Tests.cs
+++ sdk/test/Test.Client.EntityFrameworkCore/OperationsQueue_Tests.cs
@@ -40,29 +40,4 @@ public class OperationsQueue_Tests
         Assert.Equal(p3.Id, items[1].Id);
         Assert.Equal(p2.Id, items[2].Id);
     }
-
-    [Fact]
-    public async Task Returns_WithNextInOrder()
-    {
-        var p1 = new OperationsQueueEntry { Id = Guid.NewGuid(), CreatedAt = DateTimeOffset.UtcNow.AddHours(-2) };
-        var p2 = new OperationsQueueEntry { Id = Guid.NewGuid(), CreatedAt = DateTimeOffset.UtcNow };
-        var p3 = new OperationsQueueEntry { Id = Guid.NewGuid(), CreatedAt = DateTimeOffset.UtcNow.AddHours(-1) };
-        context.OperationsQueue.AddRange(p1, p2, p3);
-        await context.SaveChangesAsync();
-
-        var op = await queue.GetNextOperationAsync();
-        Assert.NotNull(op);
-        Assert.Equal(p1.Id, op!.Id);
-
-        op = await queue.GetNextOperationAsync();
-        Assert.NotNull(op);
-        Assert.Equal(p3.Id, op!.Id);
-
-        op = await queue.GetNextOperationAsync();
-        Assert.NotNull(op);
-        Assert.Equal(p2.Id, op!.Id);
-
-        op = await queue.GetNextOperationAsync();
-        Assert.Null(op);
-    }
 }
