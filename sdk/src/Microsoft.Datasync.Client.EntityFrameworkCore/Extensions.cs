a0732d84bb69faa8eeb3310d8f23d47990570175
Adrian Hall <photoadrian@outlook.com>
2023-02-15 16:10:17 -0800

    Added GetTablePropertySet() extension method

--- sdk/src/Microsoft.Datasync.Client.EntityFrameworkCore/Extensions.cs
+++ sdk/src/Microsoft.Datasync.Client.EntityFrameworkCore/Extensions.cs
@@ -7,6 +7,26 @@ namespace Microsoft.Datasync.Client.EntityFrameworkCore;
 internal static class DatasyncEFExtensions
 {
     /// <summary>
+    /// Gets the list of <see cref="PropertyInfo"/> elements that correspond to the <see cref="DbSet{T}"/>
+    /// elements in the provided <see cref="DbContext"/>
+    /// </summary>
+    /// <param name="context">The <see cref="DbContext"/> to process.</param>
+    /// <returns>A list of <see cref="PropertyInfo"/> objects representing the tables in the context.</returns>
+    internal static IList<PropertyInfo> GetTablePropertySet(this DbContext context)
+    {
+        var properties = new List<PropertyInfo>();
+        foreach (var property in context.GetTablePropertySet().GetProperties())
+        {
+            var setType = property.PropertyType;
+            if (setType.IsGenericType && (typeof (DbSet<>).IsAssignableFrom(setType.GetGenericTypeDefinition())))
+            {
+                properties.Add(property);
+            }
+        }
+        return properties;
+    }
+
+    /// <summary>
     /// Based on a specific <see cref="DbContext"/>, determine the actual table name underlying
     /// a type used in a <see cref="DbSet{T}"/>.
     /// </summary>
